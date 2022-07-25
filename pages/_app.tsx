e32e706233bb9cc31208ed32cc49fc6e1a94afe0
Micurran <mcurran1722@gmail.com>
2022-07-25 07:31:23 -0600

    remove redundant SEO add ts types to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
+import { AppProps } from 'next/app';
 import '../theme/globals.css'
 import { ChakraProvider} from "@chakra-ui/react"
 import { theme } from '../theme/theme';
 
-function App({ Component, pageProps }) {
+function App({ Component, pageProps }: AppProps) {
   return (
       <ChakraProvider theme={theme}>
           <Component {...pageProps} />
